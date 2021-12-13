b1c95b3c2047b54d75c904ccbb4901c4f2243ede
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2021-12-13 22:10:11 +0100

    Konto class

---
+++ src/konto/campus02/erfani/Konto.java
@@ -0,0 +1,18 @@
+package konto.campus02.erfani;
+
+public class Konto {
+    protected String inhaber;
+    protected double kontostand;
+
+    public Konto(String inhaber ) {
+        this.inhaber = inhaber;
+    }
+
+    public void einzahlen( double value ) {
+
+    }
+
+    public void auszahlen( double value ) {
+
+    }
+}
