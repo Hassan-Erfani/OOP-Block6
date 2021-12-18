97e37de906414768383fc2b56a84580bd803a71a
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2021-12-18 19:22:45 +0100

    GiroKonto => 0 value issue solved

--- src/konto/campus02/erfani/GiroKonto.java
+++ src/konto/campus02/erfani/GiroKonto.java
@@ -16,32 +16,29 @@ public class GiroKonto extends Konto{
 
     public void auszahlen( double value ) {
 
-                    // check if value != 0
-                    if( value == 0 ) {
-                        System.out.println( "Auszahlung nicht möglich bitte geben Sie einen Betrag größer als 0 ein." );
-                        // TODO 0 value needs to modified -> the App withdraw 0 value as well
-                    }
+        // check if value is 0
+        if (value == 0) {
+            System.out.println("Auszahlung nicht möglich bitte geben Sie einen Betrag größer als 0 ein.");
+        } else {
 
-                    // check if value is less than limit and save the limit in tmp
-                    if ( value <= this.limit && tmp < this.limit) {
-                        tmp += value;}
-                    // check if all transactions has reached the limit
-                    if ( tmp < this.limit ) {
-                        // check if value is less than balance
-                        if( value <= this.kontostand && value != 0 ) {
-                            this.kontostand -= value;
-                            System.out.println( "Der Betrag: " + value + " wurde erfolgreich abgebucht" );
-                            System.out.println( "Kontostand: " + this.kontostand ) ;
-                        } else {
-                            System.out.println( "Eine Auszahlung von " + value + " nicht möglich, Kontostand niedrig" );
-                        }
-                    } else {
-                        tmp -= value;
-                        System.out.println( "Der Betrag: " + value + ", kann nicht abgebucht werden, Limit überschritten" );
-                    }
-
-
-
-
+            // check if value is less than limit and save the limit in tmp
+            if (value <= this.limit && tmp < this.limit) {
+                tmp += value;
+            }
+            // check if all transactions has reached the limit
+            if (tmp < this.limit) {
+                // check if value is less than balance
+                if (value <= this.kontostand && value != 0) {
+                    this.kontostand -= value;
+                    System.out.println("Der Betrag: " + value + " wurde erfolgreich abgebucht");
+                    System.out.println("Kontostand: " + this.kontostand);
+                } else {
+                    System.out.println("Eine Auszahlung von " + value + " nicht möglich, Kontostand niedrig");
+                }
+            } else {
+                tmp -= value;
+                System.out.println("Der Betrag: " + value + ", kann nicht abgebucht werden, Limit überschritten");
+            }
+        }
     }
 }
